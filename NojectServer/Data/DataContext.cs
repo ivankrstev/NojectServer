2c8b91930d13f22adb9bd84612d8a85970234d05
Ivan Krstev <ivankrstev246@gmail.com>
2023-10-20 01:08:29 +0200

    Added initial Database Context for EFCore, with User DbSet

---
+++ NojectServer/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NojectServer.Models;
+
+namespace NojectServer.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
