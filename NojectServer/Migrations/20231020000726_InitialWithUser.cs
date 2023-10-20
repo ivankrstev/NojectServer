17689adca178eba93121c7d8aa5cdd6a1ffa4cdb
Ivan Krstev <ivankrstev246@gmail.com>
2023-10-20 02:11:16 +0200

    Added initial EFCore migration for DB setup and User schema

---
+++ NojectServer/Migrations/20231020000726_InitialWithUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NojectServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialWithUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(type: "character varying(62)", maxLength: 62, nullable: false),
+                    full_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    password = table.Column<byte[]>(type: "bytea", nullable: false),
+                    password_salt = table.Column<byte[]>(type: "bytea", nullable: false),
+                    verification_token = table.Column<string>(type: "char(128)", nullable: true),
+                    verified_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    password_reset_token = table.Column<string>(type: "char(128)", nullable: true),
+                    reset_token_expires = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.user_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
