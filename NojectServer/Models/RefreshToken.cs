6d79e6c23c4502e142fdb321439153aa0db610e4
Ivan Krstev <ivankrstev246@gmail.com>
2023-11-17 18:19:34 +0100

    Created RefreshToken entity model with data annotations

---
+++ NojectServer/Models/RefreshToken.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NojectServer.Models
+{
+    [Table("refresh_tokens")]
+    [PrimaryKey(nameof(Email), nameof(Token))]
+    public class RefreshToken
+    {
+        [Column("user_id")]
+        [Required]
+        [StringLength(62)]
+        public string Email { get; set; } = string.Empty;
+
+        [Column("token")]
+        [Required]
+        public string Token { get; set; } = string.Empty;
+
+        [Column("valid_until")]
+        public DateTime ExpireDate { get; set; } = DateTime.UtcNow.AddDays(14);
+    }
+}
