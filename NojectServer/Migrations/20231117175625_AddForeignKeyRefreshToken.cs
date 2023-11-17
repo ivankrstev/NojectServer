2d72ccef0ba37d066d245bd0657d1be7ead6be5c
Ivan Krstev <ivankrstev246@gmail.com>
2023-11-17 18:58:04 +0100

    Added database migrations for adding the foreign key on RefreshToken.Email

---
+++ NojectServer/Migrations/20231117175625_AddForeignKeyRefreshToken.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NojectServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyRefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_refresh_tokens_users_user_id",
+                table: "refresh_tokens",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "user_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_refresh_tokens_users_user_id",
+                table: "refresh_tokens");
+        }
+    }
+}
