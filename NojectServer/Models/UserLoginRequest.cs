b4579dabc0b54d0def04c37d6803ebb25c5640ef
Ivan Krstev <ivankrstev246@gmail.com>
2023-11-17 02:46:12 +0100

    Added model for user login request with required fields

---
+++ NojectServer/Models/UserLoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NojectServer.Models
+{
+    public class UserLoginRequest
+    {
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
