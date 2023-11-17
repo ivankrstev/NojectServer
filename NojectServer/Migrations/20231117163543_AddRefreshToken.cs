fdc485fac61c184ea8c2119b3dda1833eaf6e872
Ivan Krstev <ivankrstev246@gmail.com>
2023-11-17 18:28:52 +0100

    Added database migrations for RefreshTokens in a 'refresh_tokens' table

---
+++ NojectServer/Migrations/20231117163543_AddRefreshToken.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NojectServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "refresh_tokens",
+                columns: table => new
+                {
+                    user_id = table.Column<string>(type: "character varying(62)", maxLength: 62, nullable: false),
+                    token = table.Column<string>(type: "text", nullable: false),
+                    valid_until = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_refresh_tokens", x => new { x.user_id, x.token });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "refresh_tokens");
+        }
+    }
+}
