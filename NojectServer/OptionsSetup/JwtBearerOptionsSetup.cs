e42ff8c0a45e48b7b1d70ad10bd13014005ca825
Ivan Krstev <ivankrstev246@gmail.com>
2023-11-20 21:11:22 +0100

    Added Authentication.JwtBearer package and jwt auth options setup

---
+++ NojectServer/OptionsSetup/JwtBearerOptionsSetup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace NojectServer.OptionsSetup
+{
+    public class JwtBearerOptionsSetup
+    {
+        public void GetOptions(IConfiguration _config, JwtBearerOptions options)
+        {
+            options.TokenValidationParameters = new()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWTSecrets:AccessToken"]!)),
+                ValidateLifetime = true,
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        }
+    }
+}
