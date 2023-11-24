15b071c1a673fcbfbdfbc89499e9e0347fb57db1
Ivan Krstev <ivankrstev246@gmail.com>
2023-11-24 21:47:31 +0100

    Added model for project creation request with name property

---
+++ NojectServer/Models/CreateProjectRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NojectServer.Models
+{
+    public class CreateProjectRequest
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "The Project Name must be a string with a maximum length of 50")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
