29f53c4433a56f387775a69d3161a991cf8b3731
Ivan Krstev <ivankrstev246@gmail.com>
2023-11-24 19:42:58 +0100

    Added database migrations for Projects in a 'projects' table

---
+++ NojectServer/Migrations/20231124183740_AddProject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NojectServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "projects",
+                columns: table => new
+                {
+                    project_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    created_by = table.Column<string>(type: "character varying(62)", nullable: false),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    color = table.Column<string>(type: "char(7)", nullable: false),
+                    background_color = table.Column<string>(type: "char(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_projects", x => x.project_id);
+                    table.ForeignKey(
+                        name: "FK_projects_users_created_by",
+                        column: x => x.created_by,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_projects_created_by",
+                table: "projects",
+                column: "created_by");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "projects");
+        }
+    }
+}
