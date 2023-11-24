9276cbc31cfa6094528f677b1f717da5b0ccbd69
Ivan Krstev <ivankrstev246@gmail.com>
2023-11-24 19:40:22 +0100

    Created Project entity model with all data annotations except task ones

---
+++ NojectServer/Models/Project.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace NojectServer.Models
+{
+    [Table("projects")]
+    public class Project
+    {
+        [Column("project_id")]
+        [Key, Required]
+        public Guid Id { get; set; } = new Guid();
+
+        [Column("name")]
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Column("created_by")]
+        [ForeignKey("User")]
+        [Required]
+        public string CreatedBy { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public virtual User? User { get; set; }
+
+        [Column("created_on")]
+        [Required]
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        [Column("color", TypeName = "char(7)")]
+        [Required]
+        public string Color { get; set; } = string.Empty;
+
+        [Column("background_color", TypeName = "char(7)")]
+        [Required]
+        public string BackgroundColor { get; set; } = string.Empty;
+    }
+}
