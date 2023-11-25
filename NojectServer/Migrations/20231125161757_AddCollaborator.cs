7807416cd1e25e115b29bc30b015e2750fdc5a52
Ivan Krstev <ivankrstev246@gmail.com>
2023-11-25 22:29:00 +0100

    Added database migrations for project Collaborators in a 'collaborators' table

---
+++ NojectServer/Migrations/20231125161757_AddCollaborator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NojectServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCollaborator : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "collaborators",
+                columns: table => new
+                {
+                    project_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<string>(type: "character varying(62)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_collaborators", x => new { x.project_id, x.user_id });
+                    table.ForeignKey(
+                        name: "FK_collaborators_projects_project_id",
+                        column: x => x.project_id,
+                        principalTable: "projects",
+                        principalColumn: "project_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_collaborators_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_collaborators_user_id",
+                table: "collaborators",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "collaborators");
+        }
+    }
+}
