51d486842f02b8d15552e0ba69e2e69364930bec
Ivan Krstev <ivankrstev246@gmail.com>
2023-11-25 22:28:15 +0100

    Added project collaborator entity model with foreign key data annotations

---
+++ NojectServer/Models/Collaborator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace NojectServer.Models
+{
+    [Table("collaborators")]
+    [PrimaryKey(nameof(ProjectId), nameof(CollaboratorId))]
+    public class Collaborator
+    {
+        [Column("project_id")]
+        [ForeignKey("Project")]
+        [Required]
+        public Guid ProjectId { get; set; } = Guid.Empty;
+
+        [JsonIgnore]
+        public virtual Project? Project { get; set; }
+
+        [Column("user_id")]
+        [ForeignKey("User")]
+        [Required]
+        public string CollaboratorId { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public virtual User? User { get; set; }
+    }
+}
