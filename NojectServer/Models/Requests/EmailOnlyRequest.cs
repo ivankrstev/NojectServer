39c725cb8df086c836e5dea26e26557a3d61d49e
Ivan Krstev <ivankrstev246@gmail.com>
2023-12-01 19:48:03 +0100

    Added request model to retrieve only email from the request body

---
+++ NojectServer/Models/Requests/EmailOnlyRequest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace NojectServer.Models.Requests
+{
+    public class EmailOnlyRequest
+    {
+        [FromBody, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
