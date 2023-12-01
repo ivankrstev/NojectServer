1d37f8cc00cecd01dd7a8598bc4842b1e778b07e
Ivan Krstev <ivankrstev246@gmail.com>
2023-12-01 19:59:41 +0100

    Added reset-password link sending to the email service

--- NojectServer/Services/Email/EmailService.cs
+++ NojectServer/Services/Email/EmailService.cs
@@ -43,5 +43,31 @@ namespace NojectServer.Services.Email
             _smtp.Send(email);
             _smtp.Disconnect(true);
         }
+
+        public void SendResetPasswordLink(User user)
+        {
+            MimeMessage email = new();
+            email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailFrom").Value));
+            email.To.Add(MailboxAddress.Parse(user.Email));
+            email.Subject = "Password Reset Request";
+            string resetPasswordLink = $"{_config.GetSection("EmailClientUrl").Value}/reset-password?token={user.PasswordResetToken}";
+            TextPart textPart = new("plain")
+            {
+                Text = $"Dear {user.FullName},\n Please click on the following link to reset your password: {resetPasswordLink}"
+            };
+            TextPart htmlPart = new("html")
+            {
+                Text = $"<p style=\"font-size:16px;color:#333;margin-bottom:10px;\">Dear {user.FullName},</p><p style=\"font-size:14px;color:#555;margin-bottom:20px;\">Please click on the following link to reset your password:</p><a style=\"display:inline-block;padding:10px 20px;background-color:#337ab7;color:#fff;text-decoration:none;border-radius:4px;font-size:14px;\" href=\"{resetPasswordLink}\">Reset Password</a>"
+            };
+            Multipart multipart = new("alternative")
+            {
+                textPart,
+                htmlPart
+            };
+            email.Body = multipart;
+
+            _smtp.Send(email);
+            _smtp.Disconnect(true);
+        }
     }
 }
--- NojectServer/Services/Email/IEmailService.cs
+++ NojectServer/Services/Email/IEmailService.cs
@@ -5,5 +5,7 @@ namespace NojectServer.Services.Email
     public interface IEmailService
     {
         void SendVerificationLink(User user);
+
+        void SendResetPasswordLink(User user);
     }
 }
