324343eb4769670aa1159033ee71945747e1bcb0
Ivan Krstev <ivankrstev246@gmail.com>
2023-12-06 01:04:36 +0100

    Added database migrations for tasks in a 'tasks' table and project 'first_task' addition

---
+++ NojectServer/Migrations/20231206000337_AddedTaskWithProjectPointers.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NojectServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTaskWithProjectPointers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "first_task",
+                table: "projects",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "tasks",
+                columns: table => new
+                {
+                    task_id = table.Column<int>(type: "integer", nullable: false),
+                    project_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    level = table.Column<int>(type: "integer", nullable: false),
+                    value = table.Column<string>(type: "text", nullable: false),
+                    next = table.Column<int>(type: "integer", nullable: true),
+                    created_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    last_modified_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    completed = table.Column<bool>(type: "boolean", nullable: false),
+                    completed_by = table.Column<string>(type: "character varying(62)", nullable: true),
+                    created_by = table.Column<string>(type: "character varying(62)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tasks", x => new { x.task_id, x.project_id });
+                    table.UniqueConstraint("AK_tasks_task_id", x => x.task_id);
+                    table.ForeignKey(
+                        name: "FK_tasks_projects_project_id",
+                        column: x => x.project_id,
+                        principalTable: "projects",
+                        principalColumn: "project_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tasks_tasks_next",
+                        column: x => x.next,
+                        principalTable: "tasks",
+                        principalColumn: "task_id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_tasks_users_completed_by",
+                        column: x => x.completed_by,
+                        principalTable: "users",
+                        principalColumn: "user_id");
+                    table.ForeignKey(
+                        name: "FK_tasks_users_created_by",
+                        column: x => x.created_by,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_projects_first_task",
+                table: "projects",
+                column: "first_task");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tasks_completed_by",
+                table: "tasks",
+                column: "completed_by");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tasks_created_by",
+                table: "tasks",
+                column: "created_by");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tasks_next",
+                table: "tasks",
+                column: "next");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tasks_project_id",
+                table: "tasks",
+                column: "project_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_projects_tasks_first_task",
+                table: "projects",
+                column: "first_task",
+                principalTable: "tasks",
+                principalColumn: "task_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_projects_tasks_first_task",
+                table: "projects");
+
+            migrationBuilder.DropTable(
+                name: "tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_projects_first_task",
+                table: "projects");
+
+            migrationBuilder.DropColumn(
+                name: "first_task",
+                table: "projects");
+        }
+    }
+}
