13eae5ff11c5a871d0eb26bc4dc7fbb4a5df479e
Ivan Krstev <ivankrstev246@gmail.com>
2023-12-22 00:09:17 +0100

    Added request model for creating a new task with optional prev task id

---
+++ NojectServer/Models/Requests/CreateNewTaskRequest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NojectServer.Models.Requests
+{
+    public class CreateNewTaskRequest
+    {
+        // Get the prev task id, so the new task can be added after that one
+        [FromBody]
+        public int? Prev { get; set; }
+    }
+}
