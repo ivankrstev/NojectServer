37b24ae75ea5b3a65401e319d231bdb669a3a790
Ivan Krstev <ivankrstev246@gmail.com>
2023-12-27 02:04:08 +0100

    Added SignalR Hub for sending shared project's changes(add/remove)

---
+++ NojectServer/Hubs/SharedProjectsHub.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using StackExchange.Redis;
+using System.Security.Claims;
+
+namespace NojectServer.Hubs
+{
+    [Authorize]
+    public sealed class SharedProjectsHub : Hub
+    {
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
+
+        public SharedProjectsHub(IConnectionMultiplexer connectionMultiplexer)
+        {
+            _connectionMultiplexer = connectionMultiplexer;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var user = Context.User?.FindFirst(ClaimTypes.Name)?.Value!;
+            AddNewConnection(user, Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            var user = Context.User?.FindFirst(ClaimTypes.Name)?.Value!;
+            RemoveExistingConnection(user, Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async void AddNewConnection(string user, string connId)
+        {
+            var redisDb = _connectionMultiplexer.GetDatabase();
+            await redisDb.SetAddAsync("sharedprojects:" + user, connId);
+        }
+
+        public async void RemoveExistingConnection(string user, string connId)
+        {
+            var redisDb = _connectionMultiplexer.GetDatabase();
+            await redisDb.SetRemoveAsync("sharedprojects:" + user, connId);
+        }
+    }
+}
