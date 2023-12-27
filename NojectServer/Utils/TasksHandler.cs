3c4bfaab90c9bca70e028b2be3765c8c8b13e951
Ivan Krstev <ivankrstev246@gmail.com>
2023-12-27 21:43:03 +0100

    Added tasks handler for ordering tasks according to the next & first_task pointers

---
+++ NojectServer/Utils/TasksHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace NojectServer.Utils
+{
+    public class TasksHandler
+    {
+        public static List<Models.Task> OrderTasks(Models.Task[] unorderedTasks, int? first_task)
+        {
+            if (unorderedTasks.Length == 0 || first_task == null) return new List<Models.Task>();
+            if (unorderedTasks.Length == 1) return new List<Models.Task>() { unorderedTasks[0] };
+            int? current_next = null;
+            List<Models.Task> orderedTasks = new();
+            for (int i = 0; i < unorderedTasks.Length; i++)
+            {
+                int id = unorderedTasks[i].Id;
+                int? next = unorderedTasks[i].Next;
+                if (orderedTasks.Count == 0 && id == first_task)
+                {
+                    orderedTasks.Add(unorderedTasks[i]);
+                    unorderedTasks = unorderedTasks.Where(x => x.Id != id).ToArray();
+                    current_next = next;
+                    i = -1;
+                }
+                else if (current_next == id)
+                {
+                    orderedTasks.Add(unorderedTasks[i]);
+                    unorderedTasks = unorderedTasks.Where(x => x.Id != id).ToArray();
+                    current_next = next; // Set the pointer for the next element to be searched and added to the array
+                    i = -1;
+                    if (next == null) break;
+                }
+            }
+            return orderedTasks;
+        }
+    }
+}
