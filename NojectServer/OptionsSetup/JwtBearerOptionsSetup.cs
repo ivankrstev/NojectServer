a3f1372cbc8db27d1f8f7f5c4380b8912280a1d5
Ivan Krstev <ivankrstev246@gmail.com>
2024-01-06 19:30:15 +0100

    Added additonal authorization for signalr hubs with access_token query

--- NojectServer/OptionsSetup/JwtBearerOptionsSetup.cs
+++ NojectServer/OptionsSetup/JwtBearerOptionsSetup.cs
@@ -16,6 +16,19 @@ namespace NojectServer.OptionsSetup
                 ValidateIssuer = false,
                 ValidateAudience = false
             };
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    var accessToken = context.Request.Query["access_token"];
+                    var path = context.HttpContext.Request.Path;
+                    if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/SharedProjectsHub") || path.StartsWithSegments("/TasksHub")))
+                    {
+                        context.Token = accessToken;
+                    }
+                    return Task.CompletedTask;
+                }
+            };
         }
     }
 }
