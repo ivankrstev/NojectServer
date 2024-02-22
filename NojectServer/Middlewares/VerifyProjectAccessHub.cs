e57e4c871d93550ff53b1d0734074bea4a8f18e9
Ivan Krstev <ivankrstev246@gmail.com>
2024-02-22 19:19:37 +0100

    Added semaphore for enabling task hub methods queuing when reaching db max pool capacity

--- NojectServer/Middlewares/VerifyProjectAccessHub.cs
+++ NojectServer/Middlewares/VerifyProjectAccessHub.cs
@@ -8,7 +8,8 @@ namespace NojectServer.Middlewares
     public class VerifyProjectAccessHub : IHubFilter
     {
         private readonly DataContext _dataContext;
-        private readonly List<string> _methodsRequiringMiddleware = new() { "ProjectJoin", "AddTask" };
+        private readonly List<string> _methodsRequiringMiddleware = new() { "ProjectJoin", "AddTask", "ChangeValue" };
+        public static readonly SemaphoreSlim _semaphore = new(90, 90); // Count of concurrent users that can use task operations, so requests can wait if max pool is on limit
 
         public VerifyProjectAccessHub(DataContext dataContext)
         {
@@ -35,8 +36,17 @@ namespace NojectServer.Middlewares
 
         private async Task<bool> CheckProjectAccess(Guid projectId, string userEmail)
         {
-            return await _dataContext.Projects.AnyAsync(p => p.Id == projectId && p.CreatedBy == userEmail) ||
-                await _dataContext.Collaborators.AnyAsync(c => c.ProjectId == projectId && c.CollaboratorId == userEmail);
+            try
+            {
+                await _semaphore.WaitAsync();
+                return await _dataContext.Projects.AnyAsync(p => p.Id == projectId && p.CreatedBy == userEmail) ||
+                        await _dataContext.Collaborators.AnyAsync(c => c.ProjectId == projectId && c.CollaboratorId == userEmail);
+            }
+            catch (Exception)
+            {
+                _semaphore.Release();
+                throw new HubException("Unable to verify your access to the requested project");
+            }
         }
     }
 }
