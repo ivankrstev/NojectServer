d2c4b6efc4075ab42b5c4c9b7749d758bea58315
Ivan Krstev <ivankrstev246@gmail.com>
2025-03-05 04:23:20 +0100

    feat(auth): Implement TokenService for secure JWT authentication

---
+++ NojectServer/Services/Auth/Interfaces/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace NojectServer.Services.Auth.Interfaces;
+
+public interface ITokenService
+{
+    string CreateAccessToken(string email);
+
+    string CreateRefreshToken(string email);
+
+    string CreateTfaToken(string email);
+
+    TokenValidationParameters GetTfaTokenValidationParameters();
+}
