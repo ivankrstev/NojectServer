fae625d9e393e3101c0bbd076de703b8931d712d
Ivan Krstev <ivankrstev246@gmail.com>
2025-03-06 01:51:36 +0100

    feat(auth): Add PasswordService class and IPasswordService interface
    Implement password hashing and verification methods.
    The `PasswordService` class provides `CreatePasswordHash` and `VerifyPasswordHash` methods using HMACSHA512 for secure password handling. The `IPasswordService` interface is defined to ensure consistent implementation across services.

---
+++ NojectServer/Services/Common/Implementations/PasswordService.cs
@@ -0,0 +1,25 @@
+﻿using NojectServer.Services.Common.Interfaces;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NojectServer.Services.Common.Implementations;
+
+public class PasswordService : IPasswordService
+{
+    public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using var hmac = new HMACSHA512();
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password ?? string.Empty));
+    }
+
+    public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        // Check for null hash or salt
+        if (passwordHash == null || passwordSalt == null)
+            return false;
+        using var hmac = new HMACSHA512(passwordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password ?? string.Empty));
+        return computedHash.SequenceEqual(passwordHash);
+    }
+}
