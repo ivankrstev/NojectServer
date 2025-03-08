952b73b3df48ab2db82fd8d4a6642b3df1dc1d36
Ivan Krstev <ivankrstev246@gmail.com>
2025-03-08 20:55:18 +0100

    feat: add error handling classes
    - Add ErrorDetails for failures in Result<T> with properties for Error, Message, and StatusCode.
    - Add generic Result<T> with static Success and Failure methods.

---
+++ NojectServer/Utils/ErrorDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace NojectServer.Utils;
+
+public class ErrorDetails(string error, string message, int statusCode)
+{
+    public string Error { get; } = error;
+    public string Message { get; } = message;
+    public int StatusCode { get; } = statusCode;
+}
---
+++ NojectServer/Utils/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace NojectServer.Utils;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public T? Value { get; } // The result if successful
+    public string? Error { get; } // The error message if failed
+
+    private Result(bool isSuccess, T? value, string? error)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        Error = error;
+    }
+
+    public static Result<T> Success(T value) => new(true, value, null);
+    public static Result<T> Failure(string error) => new(false, default, error);
+}
