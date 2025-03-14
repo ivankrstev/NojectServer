960121c4656a13c16fe96939e394549864554ac2
Ivan Krstev <ivankrstev246@gmail.com>
2025-03-14 03:18:10 +0100

    fix: add forgotten using directives for collaborators service

--- NojectServer/DependencyInjection/ServiceCollectionExtensions.cs
+++ NojectServer/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using NojectServer.Middlewares;
 using NojectServer.Services.Auth.Implementations;
 using NojectServer.Services.Auth.Interfaces;
+using NojectServer.Services.Collaborators.Implementations;
+using NojectServer.Services.Collaborators.Interfaces;
 using NojectServer.Services.Common.Implementations;
 using NojectServer.Services.Common.Interfaces;
 using NojectServer.Services.Email;
